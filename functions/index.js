2f616241e29fb167e0f407f4ec7ae584f9843e7c
bastien <bastien.nicoud@gmail.com>
2019-02-12 10:32:12 +0100

    Separate new automaton function

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,10 @@
-const functions = require('firebase-functions');
+// Load firebase functions sdk
+const functions = require('firebase-functions')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Load functions
+const newAutomaton = require('./newAutomaton')
 
-exports.newAutomaton = functions.region('europe-west1').https.onCall((data, context) => {
-  console.info("newAutomaton function called")
-  return {
-    message: `Function corectly called with text : ${data.text}`
-  }
-})
+exports.newAutomaton = functions
+  .region('europe-west1')
+  .https
+  .onCall(newAutomaton)
