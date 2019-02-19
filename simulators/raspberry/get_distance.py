326e965fdbc35f8390ab37a7edf1eaa0f6a10fba
bastien <bastien.nicoud@gmail.com>
2019-02-19 11:42:02 +0100

    Clean ultarasonic sensor script

--- simulators/raspberry/get_distance.py
+++ simulators/raspberry/get_distance.py
@@ -2,33 +2,33 @@ import RPi.GPIO as GPIO
 import paho.mqtt.client as mqtt
 import time
 
+print ("Distance Measurement script !")
+
 GPIO.setmode(GPIO.BCM)
 
 TRIG = 23
 ECHO = 24
 
-print ("Distance Measurement")
-
 GPIO.setup(TRIG,GPIO.OUT)
 GPIO.setup(ECHO,GPIO.IN)
 
-loop = 0
-
 # Define mqtt events
 def on_connect(client, userdata, flags, rc):
-    print("Connexion to the proker ready")
-    
+    print("Broker connected")
+
 def on_disconnect(client, userdata, rc):
-    print("Connexion perdue")
-    
+    print("Connexion with broker lost")
+
 # Create the mqtt client
 mqtt_c = mqtt.Client('raspberry')
 
+# Assign connexion events
 mqtt_c.on_connect = on_connect
 mqtt_c.on_disconnect = on_disconnect
-    
+
 # Connecting to the mqtt broker
-mqtt_c.connect("172.17.101.124", 1883, username: "raspberry")
+mqtt_c.username_pw_set("raspberry", password=None)
+mqtt_c.connect("172.17.101.124", 1883)
 mqtt_c.loop_start()
 
 while True:
@@ -47,18 +47,19 @@ while True:
 
     while GPIO.input(ECHO)==0:
       pulse_start = time.time()
-      
+
     while GPIO.input(ECHO)==1:
       pulse_end = time.time()
-      
-    pulse_duration = pulse_end - pulse_start  
+
+    pulse_duration = pulse_end - pulse_start
 
     distance = pulse_duration * 17150
-      
+
     distance = round(distance, 2)
-    
+
+    # Send the mesured value to the Broker
     mqtt_c.publish("raspberry_ultrasonic", distance)
     print ("Distance:",distance,"cm")
 
+# Clean GPIO pins assignments
 GPIO.cleanup()
-
