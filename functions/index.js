f665d81f158e48bd1d9ecd6ee792bfb6d945e949
Kevin Jordil <kevin.jordil@cpnv.ch>
2019-02-19 07:56:44 +0100

    Update new user function

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,5 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-
-admin.initializeApp(functions.config().firebase);
 
 const newUser = require('./newUser')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
-exports.newAutomaton = functions.region('europe-west1').https.onCall((data, context) => {
-  console.info("newAutomaton function called")
-  return {
-    message: `Function corectly called with text : ${data.text}`
-  }
-})
+exports.newUser = newUser
--- functions/newUser.js
+++ functions/newUser.js
@@ -1,10 +1,23 @@
-exports.newUser = functions.auth.user().onCreate((event) => {
-    const user = event.data; // The firebase user
-    const id = user.uid;
-    const displayName = user.displayName;
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-    return admin.database().ref("/users/"+id).set({
-        name: displayName,
-        email: user.email
-    }); 
-});
+admin.initializeApp(functions.config().firebase);
+
+exports.default = functions.region('europe-west1').https.onCall((data, context) => {
+    console.log(data)
+    return admin.auth().createUser({
+        email: data.email,
+        emailVerified: true,
+        password: data.password,
+        displayName: data.email,
+        disabled: false,
+    })
+        .then(user => {
+            return {
+                response: user
+            }
+        })
+        .catch(error => {
+            throw new functions.https.HttpsError('failed to create a user')
+        })
+})
