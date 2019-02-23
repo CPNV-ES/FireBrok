82e42d9906e86e8b9a168d3ef4c1b5a735c51b7f
bastien <bastien.nicoud@gmail.com>
2019-02-23 14:51:38 +0100

    Update gitignores for idea cache folder

--- aggregator/python/aggregator.py
+++ aggregator/python/aggregator.py
@@ -1,3 +1,7 @@
+import firebase_admin
 from paho.mqtt.client import Client
+from firebase_admin import credentials
 
-from firebase_admin
+# Initialize the firebase admin sdk
+
+cred = credentials.Certificate('./token/admin_key.json')
