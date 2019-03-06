ededbe4764c73aef07cc8b5602f410ab9189ca62
bastien <bastien.nicoud@gmail.com>
2019-03-06 11:56:36 +0100

    Remove python agregator no libraries available for python implementations

--- aggregator/python/aggregator.py
+++
@@ -1,7 +0,0 @@
-import firebase_admin
-from paho.mqtt.client import Client
-from firebase_admin import credentials
-
-# Initialize the firebase admin sdk
-
-cred = credentials.Certificate('./token/admin_key.json')
